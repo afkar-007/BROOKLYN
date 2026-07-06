1f12df34831afaac881672baa3dc70cfd80eacf3
Afkar <afkarmohapub354@gmail.com>
2026-07-06 11:57:41 +0530

    Updated React app

--- src/Login.jsx
+++ src/Login.jsx
@@ -2,14 +2,29 @@ import React from 'react'
 import './LoginFolder/Login.css';
 import Pawimage from './assets/LoginPawImg.png'
 function Login() {
+  
+
+
+
+
   return (
     <>
+ <body className='Login-Body'>
+  
+ 
+
+
     <img src={Pawimage} alt="" className='login-logo '/>
-   <h1 className='app-heading-premium '>BROOKLYN</h1>
+    <h1 className='premium-heading '>BROOKLYN</h1>
+
+     
+
+  
+
+
+
     
-    
-    
-    
+    </body>
     </>
   )
 }
